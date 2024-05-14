fe52f1d6179a6cc947a7a2f059ebf5c45c6eb9d6
dhfrdh876dbs <csjcandegeneyer@hotmail.com>
2024-05-14 12:41:57 +0700

    aptos sdk project

--- index.js
+++ index.js
@@ -1,38 +1,24 @@
-function longestIncreasingPath(matrix) {
-  if (matrix.length === 0) return 0;
-  const rows = matrix.length;
-  const cols = matrix[0].length;
-  const dp = Array.from(Array(rows), () => Array(cols).fill(0));
-  let longestPath = 0;
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      longestPath = Math.max(longestPath, dfs(matrix, i, j, dp));
+function isValidSudoku(board) {
+  const rows = Array(9)
+    .fill()
+    .map(() => new Set());
+  const cols = Array(9)
+    .fill()
+    .map(() => new Set());
+  const boxes = Array(9)
+    .fill()
+    .map(() => new Set());
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      const num = board[i][j];
+      if (num === ".") continue;
+      const boxIndex = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+      if (rows[i].has(num) || cols[j].has(num) || boxes[boxIndex].has(num))
+        return false;
+      rows[i].add(num);
+      cols[j].add(num);
+      boxes[boxIndex].add(num);
     }
   }
-  return longestPath;
-  function dfs(matrix, i, j, dp) {
-    if (dp[i][j] !== 0) return dp[i][j];
-    const dirs = [
-      [0, 1],
-      [0, -1],
-      [1, 0],
-      [-1, 0],
-    ];
-    let maxPath = 1;
-    for (const dir of dirs) {
-      const newRow = i + dir[0];
-      const newCol = j + dir[1];
-      if (
-        newRow >= 0 &&
-        newRow < matrix.length &&
-        newCol >= 0 &&
-        newCol < matrix[0].length &&
-        matrix[newRow][newCol] > matrix[i][j]
-      ) {
-        maxPath = Math.max(maxPath, 1 + dfs(matrix, newRow, newCol, dp));
-      }
-    }
-    dp[i][j] = maxPath;
-    return maxPath;
-  }
+  return true;
 }
